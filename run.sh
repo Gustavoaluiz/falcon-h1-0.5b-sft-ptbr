150228fcc9574021f26428dc6107c28386e274b3
gustavoaluizbp <gustavoaluizbp@gmail.com>
2025-11-11 00:21:43 -0300

    feat: altera configs gerais

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 accelerate launch \
-    --config_file configs/zero3.yaml \
+    --config_file /home/jovyan/falcon-h1-0.5b-sft-ptbr/configs/zero3.yaml \
     sft.py \
-    --config configs/sft_full.yaml \
+    --config /home/jovyan/falcon-h1-0.5b-sft-ptbr/configs/sft_full.yaml \
     --model_name_or_path tiiuae/Falcon-H1-0.5B-Base \
     --run_name falcon-h1-0.5b-base-ptbr-sft \
-    --attn_implementation="kernels-community/flash-attn" \
-    --dataset_sample_size 200000 \
+    --attn_implementation="flash_attention_2" \
+    --dataset_sample_size 11000 \
     --dataset_eval_fraction 0.05
